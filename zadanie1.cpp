5a2dd83139b463e0e2cc5be5140e66eb6fdf488d
Szymon <szymondutka3@gmail.com>
2019-02-01 20:41:47 +0100

    adds zadanie1.cpp

--- zadanie1.cpp
+++ zadanie1.cpp
@@ -2,6 +2,37 @@
 using namespace std;
 
 int main(){
-    cout << "Hello world";
+    int podstawaTrojkata;
+    int liczbaGwiazdek;
+    int wysokoscTrojkata;
+
+    cout << "Podaj podstawe trojkata" << endl;
+    cin >> podstawaTrojkata;
+
+    // wyliczam sobie podstawe trojka na podstaie liczby gwiazdek
+    if( podstawaTrojkata % 2 == 0){
+        wysokoscTrojkata = podstawaTrojkata /2;
+        liczbaGwiazdek = 2;
+    }else{
+        wysokoscTrojkata = (podstawaTrojkata +1) /2;
+        liczbaGwiazdek = 1;
+    }
+    //
+    cout << "Wysokosc trojkata:" << wysokoscTrojkata << endl;
+
+    for(int j = 0; j < wysokoscTrojkata; j++){
+        
+        for(int z = 0; z < (podstawaTrojkata - liczbaGwiazdek) / 2 ; z++) {
+            cout << " ";
+        }
+        for(int i = 0; i < liczbaGwiazdek; i++){
+            cout << "*";
+        }
+
+        cout << "\n";
+        liczbaGwiazdek = liczbaGwiazdek + 2;
+    }
+
+    cout << "\n\n\n";
     return 0;
 }
---
+++ zadanie2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+    using namespace std;
+    int pln;
+    int usd;
+    cout << "podaj ile masz pln: ";
+    cin >> pln;
+    usd = pln * 3.72;
+    cout << "ilosc usd to " << usd << endl;
+ 
+return 0;
+}
