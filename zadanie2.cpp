4c6e0668472365bdf462b4f9c991cb62893a6f47
Szymon <szymondutka3@gmail.com>
2019-02-02 11:09:41 +0100

    adds zadanie1.cpp

--- zadanie2.cpp
+++ zadanie2.cpp
@@ -4,10 +4,23 @@ int main(){
     using namespace std;
     int pln;
     int usd;
-    cout << "podaj ile masz pln: ";
-    cin >> pln;
-    usd = pln * 3.72;
-    cout << "ilosc usd to " << usd << endl;
- 
+    int waluta;
+    int waluta2;
+    cout << "jaką masz walute USD(1) lub PLN(2)";
+    cin >> waluta;
+    cout << "podaj ile masz PLN lub USD";
+    cin >> waluta2;
+
+    if(waluta == 1){
+        waluta2 = usd;
+    }else{
+        waluta2 = pln;   
+    }
+
+    usd = 3.72 * pln;
+    pln = usd/3.72;
+
+    cout << "wynik to" << pln << endl;
+   
 return 0;
 }
